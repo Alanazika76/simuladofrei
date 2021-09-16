f7e75773229e2cf45ca339f735d7d9f79564e97d
Alan <alan.cafemania.02@gmail.com>
2021-09-16 18:22:34 -0300

    algumacoisa

--- api/src/index.js
+++ api/src/index.js
@@ -1,78 +1,91 @@
-import db from './db.js';
-import express from 'express'
+import db from './bd.js';
 import cors from 'cors'
+import express from 'express'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/matricula', async(req,resp) => {
-    try{
-        let alunos = await
-            db.tb_matricula.findAll({
-                order:[
-                    ['id_matricula', 'desc']
-                ]
-            })
-        resp.send(alunos)
-    } catch(e){
-        resp.send({erro: e.toString()})
-    }
-})
 
-app.post('/matricula', async(req,resp) => {
-    try{
-        let { nome, chamada, curso, turma} = req.body;
-        let ins = await
-            db.tb_matricula.create({
-                nm_aluno: nome,
-                nr_chamada: chamada,
-                nm_curso: curso,
-                nm_turma: turma
-            });
-        resp.send(ins);
-    } catch(e){
-        resp.send({erro: e.toString()})
-    }
-})
 
-app.put('/matricula/:id', async(req,resp) => {
-    try{
-        let {nome, chamada, curso, turma} = req.body;
-        let{id} = req.params;
-        let up = await
-            db.tb_matricula.update(
+app.get('/matricula', async (req, resp) => {
+    try {
+        let l = await db.tb_matricula.findAll({order: [['id_matricula', 'desc']]});
+        resp.send(l);
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+}) 
+
+
+app.post('/matricula', async (req, resp) => {
+    try {
+        let { nome, numero, curso, turma } = req.body;
+
+        let consulta = await db.tb_matricula.findOne({where: {nm_turma: turma, nr_chamada: numero}});
+        
+        if(consulta != null) 
+            resp.send({erro: '😀 Aluno já cadastrado!'})
+
+            if(nome == "" || nome.length < 5 || numero == "" || numero <= 0 || curso == "" || curso.length < 5 || turma == "" || turma.length <= 3 )
+            {
+                resp.send({erro: '❌ Campos inválidos!'})
+            } else {
+                if(numero <= 0) {
+                    resp.send({erro: 'O campo "Chamada" só pode conter números positivos!'})
+                } else {
+                let i = await db.tb_matricula.create({ 
+                    nm_aluno: nome,
+                    nr_chamada: numero,
+                    nm_curso: curso,
+                    nm_turma: turma
+                })
+                resp.send(i)    
+            }
+        }
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+}) 
+
+
+app.put('/matricula/:id', async (req, resp) => {
+    try {
+        let { nome, numero, curso, turma } = req.body;
+        let { id } = req.params;
+
+        if(nome == "" && nome.length < 2 || numero <= 0 || curso == "" && curso.length < 2 || turma == "" && nome.length <= 0 ) {
+            resp.send({erro: '❌ Campos inválidos!'})
+        } else {
+                let i = await db.tb_matricula.update(
                 {
                     nm_aluno: nome,
-                    nr_chamada: chamada,
+                    nr_chamada: numero,
                     nm_curso: curso,
                     nm_turma: turma
                 },
                 {
-                    where:{id_matricula: id}
-                }
-            )
-        resp.sendStatus(200);
-    } catch(e){
-        resp.send({erro: e.toString()})
+                    where: {id_matricula: id }
+                })
+                resp.sendStatus(200);
+            }
+    } catch (e) {
+        resp.send({ erro: e.toString() })
     }
-})
+}) 
 
-app.delete('/matricula/:id', async(req,resp) => {
-    try{
-        let{id} = req.params;
-        let de = await
-            db.tb_matricula.destroy(
-                {
-                    where: {id_matricula: id}
-                }
-            )
+
+app.delete('/matricula/:id', async (req, resp) => {
+    try {
+        let {id} = req.params;
+
+        let d = await db.tb_matricula.destroy({ where: { id_matricula: id }})
         resp.sendStatus(200);
-    } catch(e){
-        resp.send({erro: e.toString()})
+    } catch (e) {
+        resp.send({ erro: e.toString() })
     }
-})
+}) 
 
 
 app.listen(process.env.PORT,
-    x => console.log(`Server up at port ${process.env.PORT}`))
+            x => console.log(`Subiu a Api! ${process.env.PORT}`))
